234b1064963d5da8fd6f965bdf1d3f11aa60d40f
Ashby Collado <ashbycollado@gmail.com>
2025-06-12 18:50:31 -0400

    Update astro.config.mjs
    Enable experimental.session for Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,19 @@
 import mdx from '@astrojs/mdx';
+import netlify from '@astrojs/netlify';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 import siteConfig from './src/data/site-config';
 
-import netlify from '@astrojs/netlify';
-
-// https://astro.build/config
 export default defineConfig({
   site: siteConfig.website,
 
+  experimental: {
+    session: true, // ✅ enable this to resolve the build error
+  },
+
   vite: {
-      plugins: [tailwindcss()]
+    plugins: [tailwindcss()]
   },
 
   integrations: [mdx(), sitemap()],
